6294c5327d915de0ae1894df6bc084eae684a76d
Louis Ramos <louisramosdev@gmail.com>
2025-04-26 22:23:06 +0200

    refactor(models): ApplicationRecord#error_codes

--- api/app/models/application_record.rb
+++ api/app/models/application_record.rb
@@ -2,13 +2,6 @@ class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   def error_codes
-    errors.group_by_attribute.each do |_attribute, errors|
-      errors.map! do |error|
-        case error.type
-        when :other_than then :"#{error.type}_#{error.options[:count]}"
-        else error.type
-        end
-      end
-    end
+    errors.group_by_attribute.transform_values { |err| err.map(&:type) }
   end
 end
