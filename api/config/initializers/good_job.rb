4fe3b8d8cf194cae4cb8704a1def118bf2c3c295
Louis Ramos <louisramosdev@gmail.com>
2025-04-27 19:36:10 +0200

    feat(steam): create CRON task for Steam::SyncJob

--- api/config/initializers/good_job.rb
+++ api/config/initializers/good_job.rb
@@ -8,4 +8,13 @@ Rails.application.configure do
 
   config.good_job.execution_mode = Rails.env.test? ? :inline : :async
   config.good_job.preserve_job_records = false
+
+  config.good_job.enable_cron = true
+  config.good_job.cron = {
+    steam_sync: {
+      cron: "0 0 * * *",
+      class: "Steam::SyncJob",
+      description: "Fetching Steam for recently played games every day"
+    },
+  }
 end
