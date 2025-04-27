6eff2c3432622b8eefc289f2586909d4128007e8
Louis Ramos <louisramosdev@gmail.com>
2025-04-27 09:20:09 +0200

    chore(deps): install webmock

--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -13,6 +13,8 @@ require "rspec/rails"
 require "simplecov"
 SimpleCov.start "rails"
 
+require "webmock/rspec"
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
